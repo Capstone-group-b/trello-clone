d5c3cd6d62a045f03ada483cfc0a471beaa66764
EightFiveZeroEight <business.8508@gmail.com>
2023-03-17 18:35:41 -0400

    Change db.js config file

--- server/db/db.js
+++ server/db/db.js
@@ -5,6 +5,8 @@ const databaseName = pkg.name + '_db' + (process.env.NODE_ENV === 'test' ? '-tes
 
 const config = {
   logging: console.log(),
+  password: 'password',
+  username: 'root',
 };
 
 if (process.env.LOGGING === 'true') {
